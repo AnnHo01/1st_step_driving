11d08022e7f001cb03dc28104594c0a6aed91578
aho1304 <aho@artisreit.com>
2023-05-19 10:30:59 -0500

    Move google tag to head

--- src/app/pages/_document.js
+++ src/app/pages/_document.js
@@ -11,11 +11,6 @@ class MyDocument extends Document {
         <link href="https://fonts.googleapis.com/css?family=Playfair%20Display:100,200,300,400,500,600,700,800,900&display=swap" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css?family=Roboto:100,200,300,400,500,600,700,800,900&display=swap" rel="stylesheet" />
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/animate.css@3.5.2/animate.min.css" />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
         <Script src="https://www.googletagmanager.com/gtag/js?id=AW-11180674865" strategy="afterInteractive"></Script>
         <Script id="google-ads-tag" strategy="afterInteractive">
           {`window.dataLayer = window.dataLayer || [];
@@ -24,6 +19,11 @@ class MyDocument extends Document {
 
           gtag('config', 'AW-11180674865');`}
         </Script>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
       </Html>
     )
   }
