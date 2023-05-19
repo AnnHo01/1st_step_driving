0671ed1f68d799c24a6674660c78d3b40f68f6db
aho1304 <aho@artisreit.com>
2023-05-19 10:37:40 -0500

    Move google tag to under head

--- src/app/pages/_document.js
+++ src/app/pages/_document.js
@@ -8,18 +8,18 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-        <link href="https://fonts.googleapis.com/css?family=Playfair%20Display:100,200,300,400,500,600,700,800,900&display=swap" rel="stylesheet" />
-        <link href="https://fonts.googleapis.com/css?family=Roboto:100,200,300,400,500,600,700,800,900&display=swap" rel="stylesheet" />
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/animate.css@3.5.2/animate.min.css" />
-        </Head>
-        <Script src="https://www.googletagmanager.com/gtag/js?id=AW-11180674865" strategy="afterInteractive"></Script>
-        <Script id="google-ads-tag" strategy="afterInteractive">
-          {`window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
+          <Script src="https://www.googletagmanager.com/gtag/js?id=AW-11180674865" strategy="afterInteractive"></Script>
+          <Script id="google-ads-tag" strategy="afterInteractive">
+            {`window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
 
-          gtag('config', 'AW-11180674865');`}
-        </Script>
+            gtag('config', 'AW-11180674865');`}
+          </Script>
+          <link href="https://fonts.googleapis.com/css?family=Playfair%20Display:100,200,300,400,500,600,700,800,900&display=swap" rel="stylesheet" />
+          <link href="https://fonts.googleapis.com/css?family=Roboto:100,200,300,400,500,600,700,800,900&display=swap" rel="stylesheet" />
+          <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/animate.css@3.5.2/animate.min.css" />
+        </Head>
         <body>
           <Main />
           <NextScript />
