76daa34a0502a1313d229ba82593c9863805bbb8
aho1304 <aho@artisreit.com>
2023-05-19 10:48:08 -0500

    Move google tag to under body

--- src/app/pages/_document.js
+++ src/app/pages/_document.js
@@ -8,19 +8,19 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <Script src="https://www.googletagmanager.com/gtag/js?id=AW-11180674865" strategy="afterInteractive"></Script>
-          <Script id="google-ads-tag" strategy="afterInteractive">
-            {`window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-
-            gtag('config', 'AW-11180674865');`}
-          </Script>
           <link href="https://fonts.googleapis.com/css?family=Playfair%20Display:100,200,300,400,500,600,700,800,900&display=swap" rel="stylesheet" />
           <link href="https://fonts.googleapis.com/css?family=Roboto:100,200,300,400,500,600,700,800,900&display=swap" rel="stylesheet" />
           <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/animate.css@3.5.2/animate.min.css" />
         </Head>
         <body>
+          <Script src="https://www.googletagmanager.com/gtag/js?id=AW-11180674865" strategy="afterInteractive"></Script>
+          <Script id="google-ads-tag" strategy="afterInteractive" dangerouslySetInnerHTML={{
+            __html: `window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'AW-11180674865');`,
+          }} />
           <Main />
           <NextScript />
         </body>
